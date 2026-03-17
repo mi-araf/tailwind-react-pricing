cead76c31c9ab5e22b771fe8afcd055a43f44a55
mi-araf <arafmushfiq@gmail.com>
2026-03-17 09:48:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+
+	return (
+		<>
+
+			<header>
+				<DaisyNav></DaisyNav>
+			</header>
+
+		</>
+	)
+}
+
+export default App
