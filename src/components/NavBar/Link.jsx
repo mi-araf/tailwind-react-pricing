e6aaddae55a5f3bcbb875968cabb008faaa301d4
mi-araf <arafmushfiq@gmail.com>
2026-03-17 10:57:33 +0600

    menu toggle created

--- src/components/NavBar/Link.jsx
+++ src/components/NavBar/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = ({ route }) => {
     return (
-        <li className='mr-10'>
+        <li className='lg:mr-10 px-8 py-2 hover:bg-amber-900 cursor-pointer'>
            <a href={route.urlPath}>{route.name}</a> 
         </li>
     );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 
 const navigationData = [
     {
@@ -30,12 +31,22 @@ const navigationData = [
 ]
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false);
+
+    const Links = navigationData.map(route => <Link key={route.id} route={route}></Link>);
+
     return (
-        <nav>
-            <ul className='flex'>
-                {
-                    navigationData.map(route => <Link key={route.id} route={route}></Link>)
-                }
+        <nav className='flex justify-between mx-10 mt-4'>
+            <span className='flex' onClick={() => setOpen(!open)}>
+                { open ? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu> }
+
+                <ul className={`md:hidden absolute duration-1000 z-99 ${open ? 'top-12' : '-top-54' } bg-amber-700`}>
+                    {Links}
+                </ul>
+                <h3 className='ml-5 md:mt-2'>My Navbar</h3>
+            </span>
+            <ul className='md:flex hidden'>
+                {Links}
             </ul>
 
             {/* <ul className='flex'>
@@ -49,6 +60,8 @@ const NavBar = () => {
                 <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li>
             </ul> */}
+
+            <button>Sign In</button>
         </nav>
     );
 };
