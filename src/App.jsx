f129e300846282674a635831eba19571045a67b8
mi-araf <arafmushfiq@gmail.com>
2026-03-17 10:18:13 +0600

    taking lucide react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
 		<>
 
 			<header>
+				<NavBar></NavBar>
 				<DaisyNav></DaisyNav>
 			</header>
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import Link from './Link';
+
+const navigationData = [
+    {
+        id: 1,
+        name: "Home",
+        urlPath: "/"
+    },
+    {
+        id: 2,
+        name: "About",
+        urlPath: "/about"
+    },
+    {
+        id: 3,
+        name: "Services",
+        urlPath: "/services"
+    },
+    {
+        id: 4,
+        name: "Blog",
+        urlPath: "/blog"
+    },
+    {
+        id: 5,
+        name: "Contact",
+        urlPath: "/contact"
+    }
+]
 
 const NavBar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul className='flex'>
+                {
+                    navigationData.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+
+            {/* <ul className='flex'>
+                {
+                    navigationData.map(route => <li className='mr-10' key={route.id}><a href="{route.urlPath}">{route.name}</a></li>)
+                }
+            </ul> */}
+
+            {/* <ul className='flex'>
+                <li className='mr-10'><a href="/">Home</a></li>
+                <li className='mr-10'><a href="/about">About</a></li>
+                <li className='mr-10'><a href="/blog">Blog</a></li>
+            </ul> */}
+        </nav>
     );
 };
 
